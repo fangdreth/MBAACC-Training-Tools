a8e2fafbe7364838dd5f056db9852f7ec25b6686
Will Hildreth <willhildreth96@gmail.com>
2023-12-13 10:59:26 -0600

    Getting ready to release

--- RepReplay.py
+++ RepReplay.py
@@ -12,6 +12,8 @@ import warnings
 import signal
 import sys
 
+DEBUG = True
+
 wintypes = ctypes.wintypes
 windll = ctypes.windll
 create_string_buffer = ctypes.create_string_buffer
@@ -89,28 +91,47 @@ def wrapup():
         pass
     if os.path.exists("ReplayVS\\$"):
         shutil.rmtree("ReplayVS\\$")
-    input("Press any key to close...")
+    vprint("Press any key to close...", True)
     sys.exit()
-
+    
+def vprint(s, hold=False):
+    if DEBUG:
+        # I know this is inefficient
+        f = open("RepRelpay.log", "a")
+        f.write(s + "\n")
+    if hold:
+        input(s)
+    else:
+        print(s)
+        
 class Parameter:
     def __init__(self, byteLength, address):
         self.address = address
         self.num = 0
         self.b_dat = create_string_buffer(byteLength)
-          
+         
+class PlayerStruct:
+    def __init__(self):
+        self.oldScore = 0
+        self.oldX = 0
+        self.oldY = 0
+        self.oldHealth = 0
+        self.oldMeter = 0
+        self.oldScore = 0
+         
 def main():
 
     # ensure cccaster is in the same directory
     if len([name for name in os.listdir() if name.startswith("cccaster") and name.endswith(".exe")]) == 0:
-        print("Move this executable into the same directory as CCCaster before launching it")
+        vprint("Move this executable into the same directory as CCCaster before launching it")
         wrapup()
     
     # ask for a folder
-    print("Click and drag your folder of replays onto this window then press Enter")
+    vprint("Click and drag your folder of replays onto this window then press Enter")
     replayPath = input("==> ").strip("\"")
-    print(replayPath)
+    vprint(replayPath)
     if not os.path.exists(replayPath):
-        print("Unable to load given replay folder")
+        vprint("Unable to load given replay folder")
         wrapup()
 
     # Copy given replays to CCCCaster location relpay folder in a new folder called $
@@ -120,19 +141,19 @@ def main():
         shutil.rmtree("ReplayVS\\$")
     try:
         shutil.copytree(replayPath , "ReplayVS\\$")
-        input("Replays Copied")
+        vprint("Replays Copied")
     except:
-        input("Failed to copy replays")
+        vprint("Failed to copy replays")
+        wrapup()
     replayTotal = len(os.listdir("ReplayVS\\$"))
 
-    
 
     # launch cccaster
     os.startfile([name for name in os.listdir() if name.startswith("cccaster") and name.endswith(".exe")][0])
 
     # wait for cccaster to open
     os.system('cls')
-    print("Waiting for CCCaster to open...")
+    vprint("Please wait for for CCCaster to open...")
     cccasterPid = 0
     while cccasterPid == 0:
         cccasterPid = getPid("cccaster", ".exe")
@@ -145,7 +166,7 @@ def main():
 
     # wait for replay mode to launch
     os.system('cls')
-    print("Waiting for MBAA to open...")
+    vprint("Please wait for MBAA to open...")
     meltyPid = 0
     while meltyPid == 0:
         meltyPid = getPid("MBAA.exe")
@@ -154,26 +175,15 @@ def main():
     # give the user a pep talk
     time.sleep(2)
     os.system('cls')
-    print("Open your recording software of choice now")
-    print("")
-    print("Unplug any controllers you may have --")
-    print("Or at least ensure you do not have a controller as player 1 in the f4 menu")
-    print("")
-    print("To begin playback immediately, just press Enter now,")
-    print("or enter a number of seconds to delay and then press Enter")
-    optionalDelay = input("==> ")
-    if optionalDelay != "":
-        try:
-            optionalDelay = abs(int(optionalDelay))
-            for i in range(optionalDelay):
-                os.system('cls')
-                print("Playback beginning in " + str(optionalDelay - i) + " seconds")
-                time.sleep(1)
-        except:
-            print("Invalid number entered")
-            wrapup()
+    vprint("Open your recording software of choice now")
+    vprint("")
+    vprint("Make sure Player 1 is not assigned to a controller in the F4 menu")
+    vprint("")
+    vprint("Make sure Melty is NOT in fullscreen")
+    vprint("")
+    vprint("To begin playback, press Enter now", True)
     os.system('cls')
-    print("Playback has begun")
+    vprint("Playback has begun.  Please do wait for the replay to finish.")
 
     # re-focus MBAA
     focusApp(meltyPid)
@@ -234,6 +244,7 @@ def main():
     DESYNC_THRESHOLD = 30
 
     # navigate the replay menu
+    vprint("Total replays: " + str(replayTotal))
     for currentRep in range(replayTotal):
         pressKey('a')   # select $ folder
         pressKey('down')# get off the "go up" folder
@@ -246,16 +257,8 @@ def main():
         pressKey('a')   # skip intro quote
 
         # initialize tracking variables.  these are used for detecting desyncs
-        oldP1Score = 0
-        oldP2Score = 0
-        oldP1X = 0
-        oldP1Y = 0
-        oldP1Health = 0
-        oldP1Meter = 0
-        oldP2X = 0
-        oldP2Y = 0
-        oldP2Health = 0
-        oldP2Meter = 0
+        P1 = PlayerStruct()
+        P2 = PlayerStruct()
         desyncCounter = 0
         
         # get these fresh to avoid carry-over values from the last recording
@@ -267,18 +270,20 @@ def main():
             
             # bail if melty is closed
             if getPid("MBAA.exe") == 0:
-                break
+                wrapup()
         
             getParameter(p1ParaScore, programHandle, baseAddress)
             getParameter(p2ParaScore, programHandle, baseAddress)
             
             # detect a round ending
-            if oldP1Score != p1ParaScore.num or oldP2Score != p2ParaScore.num:
-                oldP1Score = p1ParaScore.num
-                oldP2Score = p2ParaScore.num
+            if P1.oldScore != p1ParaScore.num or P2.oldScore != p2ParaScore.num:
+                vprint("Round end detected P1-" + str(p1ParaScore.num) + " P2-" + str(p2ParaScore.num))
                 
-                # clean up in case there was a desync
+                P1.oldScore = p1ParaScore.num
+                P2.oldScore = p2ParaScore.num
+                
                 desyncCounter = 0
+                
                 time.sleep(1)
                 
                 pressKey('a')   # skip win quote
@@ -293,14 +298,15 @@ def main():
             getParameter(p2ParaY, programHandle, baseAddress)
             getParameter(p2ParaHealth, programHandle, baseAddress)
             getParameter(p2ParaMeter, programHandle, baseAddress)
-            if (oldP1X != p1ParaX.num or oldP2X != p2ParaX.num or
-                oldP1Y != p1ParaY.num or oldP2Y != p2ParaY.num or
-                oldP1Health != p1ParaHealth.num or oldP2Health != p2ParaHealth.num or
-                oldP1Meter != p1ParaMeter.num or oldP2Meter != p2ParaMeter.num):
+            if (P1.oldX != p1ParaX.num or P2.oldX != p2ParaX.num or
+                P1.oldY != p1ParaY.num or P2.oldY != p2ParaY.num or
+                P1.oldHealth != p1ParaHealth.num or P2.oldHealth != p2ParaHealth.num or
+                P1.oldMeter != p1ParaMeter.num or P2.oldMeter != p2ParaMeter.num):
                 desyncCounter = 0    
             else:
                 desyncCounter += 1
                 if desyncCounter == DESYNC_THRESHOLD:
+                    vprint("Desync Detected")
                     if (p1ParaHealth.num > p2ParaHealth.num and p1ParaScore == 1) or (p2ParaHealth.num > p1ParaHealth.num and p2ParaScore == 1):
                         pressKey('s')       # pause
                         pressKey('down')
@@ -319,26 +325,27 @@ def main():
                         pressKey('down')
                         pressKey('a')       # next round
                 
-            oldP1X = p1ParaX.num
-            oldP1Y = p1ParaY.num
-            oldP1Health = p1ParaHealth.num
-            oldP1Meter = p1ParaMeter.num
-            oldP2X = p2ParaX.num
-            oldP2Y = p2ParaY.num
-            oldP2Health = p2ParaHealth.num
-            oldP2Meter = p2ParaMeter.num
+            P1.oldX = p1ParaX.num
+            P1.oldY = p1ParaY.num
+            P1.oldHealth = p1ParaHealth.num
+            P1.oldMeter = p1ParaMeter.num
+            P2.oldX = p2ParaX.num
+            P2.oldY = p2ParaY.num
+            P2.oldHealth = p2ParaHealth.num
+            P2.oldMeter = p2ParaMeter.num
             
             time.sleep(0.1)
             
         # exit a finished replay
-        if not desynced:
-            pressKey('a')   # skip game end quote
-            pressKey('down')# highlight Replay Selection
-            pressKey('a')   # select Replay Selection
+        vprint("Replay " + str(currentRep) + " finished")
+        pressKey('a')   # skip game end quote
+        pressKey('down')# highlight Replay Selection
+        pressKey('a')   # select Replay Selection
         time.sleep(1)
         
         if getPid("MBAA.exe") == 0:
-                break
+            input("MBAA closed")
+            break
 
     wrapup()
 
